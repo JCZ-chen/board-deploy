6fc701fe828096ba99a09e5b869efc430ad4c00b
chen <chen@digital.local>
2026-09-08 15:04:15 +0800

    注册跨线程元类型,修复dataReady被静默丢弃

--- qt_app_src/app/main.cpp
+++ qt_app_src/app/main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include "ui/mainwindow.h"
+#include "comms/metersnapshot.h"
 
 // 用法: oilmeter [串口] [波特率] [地址列表,逗号分隔] [DE_GPIO]
 //   例: ./oilmeter /dev/ttymxc1 9600 1         (485-1, DE=gpio22)
@@ -7,6 +8,10 @@
 //        ./oilmeter /dev/ttymxc2 9600 2 23     (485-2, DE=gpio23)
 int main(int argc, char** argv)
 {
+    // 跨线程 queued 信号携带自定义类型必须先注册, 否则 dataReady 信号被静默丢弃
+    qRegisterMetaType<MeterSnapshot>("MeterSnapshot");
+    qRegisterMetaType<uint8_t>("uint8_t");
+    qRegisterMetaType<uint16_t>("uint16_t");
     QApplication app(argc, argv);
     MainWindow w;
     w.resize(900, 600);
